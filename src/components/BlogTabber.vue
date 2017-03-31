fe9a4d25d58222aaa5584930899660a34a361b03
lsc <651936960@qq.com>
2017-03-31 23:38:39 +0800

    Initial commit

---
+++ src/components/BlogTabber.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <group>
+      <cell is-link title="Simple" link="/"></cell>
+      <cell is-link title="Switch icons" link="/"></cell>
+    </group>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/demo/icon_nav_button.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/demo/icon_nav_msg.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item selected link="/">
+        <img slot="icon" src="../assets/demo/icon_nav_article.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/demo/icon_nav_cell.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem, Group, Cell } from 'vux'
+export default {
+  name: 'blogTabber',
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  }
+}
+</script>
